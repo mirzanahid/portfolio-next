0333348d6acfb60c8ee90c45feb54d5d96c2a099
mirzanahid <mirzasaikat3@gmail.com>
2025-06-19 21:02:13 +0600

    added meta data for social thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,41 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Mirza Nahid",
-  description: "Its a Mirza Nahid's Portfolio",
+  description: "I'ts a Mirza Nahid's Portfolio who is a Mern stack developer.",
   icons: {
     icon: '/mn-favicon.svg',
   },
+
+  openGraph: {
+    title: 'Mirza Nahid',
+    description: "I'ts a Mirza Nahid's Portfolio who is a Mern stack developer",
+    url: 'https://mirzanahid.vercel.app/',
+    siteName: "Mirza Nahid's Portfolio",
+    images: [
+      {
+        url: 'https://mirzanahid.vercel.app/thumbnail.png', // 🔗 Your thumbnail image (absolute URL)
+        width: 1200,
+        height: 630,
+        alt: "Mirza Nahid's Portfolio Thumbnail",
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Mirza Nahid',
+    description: "I'ts a Mirza Nahid's Portfolio who is a Mern stack developer.",
+    images: ['https://mirzanahid.vercel.app/thumbnail.png'],
+  },
+
 };
+
+
+
+
+
+
 
 export default function RootLayout({
   children,
